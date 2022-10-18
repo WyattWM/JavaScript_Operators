3d52c8cd9965189d66acf31e78678deedd9c4abe
wyattwm <wyattwmwm@gmail.com>
2022-10-18 09:34:13 -0600

    conoslelog

--- app.js
+++ app.js
@@ -6,14 +6,47 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
-// YOUR CODE HERE
+let a = 20;
+let b = 4;
+let sum = a + b;
+let minus = a - b;
+let multiplly = a * b ;
+let dividing = a / b;
+let remainder = a % b;
+let exp = a ** b;
+
+console.log(a += b);
+console.log(a -= b);
+console.log(a *= b);
+console.log(a /= b);
+console.log(a %= b);
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
-// YOUR CODE HERE
+let num = 11;
+let str = "11"
+let str2 = "eleven"
+let isPresent = true;
+let firstName = "Frodo"
+let lastName = "Baggins"
+
+console.log(num + str);
+console.log(num + str2)
+console.log(num + isPresent)
+console.log(isPresent + str)
+console.log(firstName + num)
+console.log(firstName + lastName)
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
-// YOUR CODE HERE
+console.log("val"=="str");
+console.log("val"==="str");
+console.log(!isPresent);
+console.log("eleven"=="str2"&&"val">="str");
+console.log(!isPresent || isPresent);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
