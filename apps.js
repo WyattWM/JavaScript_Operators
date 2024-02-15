38b6142ac6fa89565f9f722a9413528ca40e0455
wyattwm <wyattwmwm@gmail.com>
2024-02-15 16:01:28 -0700

    new file

---
+++ apps.js
@@ -0,0 +1,47 @@
+// Exercise 1 //
+
+var a = 20;
+var b = 4;
+var add = a + b;
+var minus = a - b;
+var multiply = a * b;
+var dividing = a / b;
+
+console.log(add);
+
+
+// Exercise 2 //
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+// Questions // 
+console.log(num + str);
+console.log(num + str2);
+console.log(num + isPresent);
+console.log(firstName + num);
+console.log(isPresent + str);
+console.log(firstName + lastName);
+
+
+// Exercise 3 //
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+// Questions //
+console.log("val" == "str3");
+console.log("val" === "str3");
+console.log(!isAwake);
+console.log("eleven" == str4 && val >= str3);
+console.log(!isAwake || isAwake);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
